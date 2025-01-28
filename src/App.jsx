506bb60bcc5b790f9ec28b036ec468c9eeb43a9c
ShernOC <sherlynea1994@hotmail.com>
2025-01-28 15:57:45 +0300

    able to connect the frontend with backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // hold the data
+  const [error, setError] = useState(null); 
+  // State to hold errors
+  const [data,setData] =  useState(null)
+  // appi variable
+  const apiUrl = 'http://127.0.0.1:5000';
+
+  // Backend Communication with Cors2
+  useEffect(() => {
+    fetch(`${apiUrl}/`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((json) => setData(json))
+      .catch((err) => setError(err.message));
+  }, [apiUrl]);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className=' text-3xl text-emerald-500 font-bold'>Collaborative Blog Platform </h1>
+      <p>Loading...
+        Success the app is loading </p>
+        
+      {error && <p style={{ color: 'red' }}>Error: {error}</p>}
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : ( 
+        <p>Loading...
+        Success the app is loading </p>
+      )}
+
+     </div>
     </>
   )
 }
